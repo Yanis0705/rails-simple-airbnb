96fee6ad80dc6c199b916c04b475fe82c8e3ba81
Yanis Chaigneau <yanis.chaigneau4@orange.fr>
2019-08-07 17:12:51 +0200

    Add database seed, model and controller flat

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "creating flat"
+
+attributes_flat =
+  {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double
+  bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guest: 3
+  }
+  {
+    name: 'Maison de bilbon',
+    address: 'la comté',
+    description: 'Idéal pour les personnes de petite taille, ne pas dépasse
+    les 1m',
+    price_per_night: 120,
+    number_of_guest: 1
+  }
+  {
+    name: 'Refuge de Yoda',
+    address: 'située quelque par sur Dagoba',
+    description: 'Fait pour les mecs qui aime bien s emmerder et pour ne pas
+    croiser de Jedis',
+    price_per_night: 10,
+    number_of_guest: 1
+  }
+  {
+    name: 'Villa de scarface',
+    address: 'Miami beach',
+    description: 'Ideal pour les aventuriers qui n ont pas peur de
+    se faire buter',
+    price_per_night: 2500,
+    number_of_guest: 10
+  }
+
+Flat.create!(attributes_flat)
+
+puts "finished!"
